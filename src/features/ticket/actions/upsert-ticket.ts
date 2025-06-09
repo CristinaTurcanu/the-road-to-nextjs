e728fdb69d1894e4f2fa3fcda3a3e589a6a08132
Cristina Turcanu <cristina.turcanu@infoworld.md>
2025-06-09 15:40:23 +0300

    Implement form validation

--- src/features/ticket/actions/upsert-ticket.ts
+++ src/features/ticket/actions/upsert-ticket.ts
@@ -2,26 +2,41 @@
 
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import {z} from 'zod';
 
 import prisma from "@/lib/prisma";
 import { ticketPath, ticketsPath } from "@/paths";
 
-export const upsertTicket = async (id: string | undefined, formData: FormData) => {
-    const data = {
-        title: formData.get("title") as string,
-        content: formData.get("content") as string,
+const upsertTicketSchema = z.object({
+    title: z.string().min(1).max(191),
+    content: z.string().min(1).max(1024)
+})
+export const upsertTicket = async (
+    id: string | undefined,
+    _actionState: { message: string, payload?: FormData }, 
+    formData: FormData
+) => {
+    try {
+        const data = upsertTicketSchema.parse({
+            title: formData.get("title"),
+            content: formData.get("content"),
+        })
+
+        await prisma.ticket.upsert({
+            where: { id: id ? id : "" },
+            update: data,
+            create: data
+        });
+    } catch { 
+        return {
+            message: 'Something went wrong!',
+            payload: formData
+        }
     }
-
-    await prisma.ticket.upsert({
-        where: { id: id ? id : "" },
-        update: data,
-        create: data
-    });
-
     revalidatePath(ticketsPath());
     if (id) {
         redirect(ticketPath(id)); 
-    } else {
-        redirect(ticketsPath());
     }
+
+    return { message: 'Ticket created' };
 };
--- src/features/ticket/components/ticket-upsert-form.tsx
+++ src/features/ticket/components/ticket-upsert-form.tsx
@@ -1,6 +1,8 @@
+"use client";
 import { Ticket } from "@prisma/client";
+import { useActionState } from "react";
 
-import { Button } from "@/components/ui/button";
+import { SubmitButton } from "@/components/form/submit-btn";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
@@ -11,16 +13,39 @@ type TicketUpsertFormProps = {
 };
 
 const TicketUpsertForm = ({ticket}: TicketUpsertFormProps) => {
+    //Option 1: Use useTransition for optimistic updates
+    // This is useful if you want to show a loading state while the action is being processed.
+
+    // const [isPending, startTransition] = useTransition();
+
+    // const upsertTicketAction = (formData: FormData) => {
+    //     startTransition(async () => {
+    //         await upsertTicket.bind(null, ticket?.id)(formData);
+    //     })
+    // };
+
+    const [actionState, action] = useActionState(
+        upsertTicket.bind(null, ticket?.id),
+        {
+            message: ''
+        }
+    );
+
     return (
-        <form action={upsertTicket.bind(null, ticket?.id)} className="flex flex-col gap-y-2">
+        <form action={action} className="flex flex-col gap-y-2">
 
             <Label htmlFor="title">Title</Label>
-            <Input id="title" name="title" type="text" defaultValue={ticket?.title}/>
+            <Input id="title" name="title" type="text" defaultValue={
+                (actionState.payload?.get('title') as string) ?? ticket?.title
+            }/>
 
             <Label htmlFor="content">Content</Label>
-            <Input id="content" name="content" defaultValue={ticket?.content}/>
+            <Input id="content" name="content" defaultValue={
+                (actionState.payload?.get('content') as string) ?? ticket?.content
+            }/>
 
-            <Button type="submit">{ticket ? 'Edit' : 'Create'}</Button>
+            <SubmitButton label={ticket ? 'Edit' : 'Create'} />
+            {actionState.message && actionState.message}
         </form>
     );
 }
