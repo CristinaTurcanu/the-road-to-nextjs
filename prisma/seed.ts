1c92252fb74110a6a336dcee5d713b42d26c1c5a
Cristina Turcanu <cristina.turcanu@infoworld.md>
2025-07-05 18:33:35 +0300

    Fix prisma seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,10 +2,10 @@ import { PrismaClient, TicketStatus} from '@prisma/client'
 const prisma = new PrismaClient();
 
 const tickets = [
-    { title: 'Ticket 1', content: 'content for ticket 1', status: 'OPEN' as TicketStatus },
-    { title: 'Ticket 2', content: 'content for ticket 2', status: 'CLOSED' as TicketStatus },
-    { title: 'Ticket 3', content: 'content for ticket 3', status: 'OPEN' as TicketStatus },
-    { title: 'Ticket 4', content: 'content for ticket 4', status: 'IN_PROGRESS' as TicketStatus },
+    { title: 'Ticket 1', content: 'content for ticket 1', status: 'OPEN' as TicketStatus, deadline: "2024-10-10", bounty: 100 },
+    { title: 'Ticket 2', content: 'content for ticket 2', status: 'CLOSED' as TicketStatus, deadline: "2024-10-10", bounty: 200 },
+    { title: 'Ticket 3', content: 'content for ticket 3', status: 'OPEN' as TicketStatus, deadline: "2024-10-10", bounty: 150 },
+    { title: 'Ticket 4', content: 'content for ticket 4', status: 'IN_PROGRESS' as TicketStatus, deadline: "2024-10-10", bounty: 250 },
 ]
 
 const seed = async () => {
