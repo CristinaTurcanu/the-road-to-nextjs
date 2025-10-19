6f9ac73848913abee51f8389bd1321d10987fc95
Cristina Turcanu <turcanu.cristina.v@gmail.com>
2025-10-19 21:03:40 +0300

    GET ticket

--- src/app/api/tickets/[ticketId]/route.ts
+++ src/app/api/tickets/[ticketId]/route.ts
@@ -2,7 +2,7 @@ import { getTicket } from "@/features/ticket/queries/get-ticket";
 
 export async function GET (
     _request: Request,
-    { params }: { params: { ticketId: string } }
+    { params }: { params: Record<string, string> }
 ) {
     const ticket = await getTicket(params.ticketId);
     
